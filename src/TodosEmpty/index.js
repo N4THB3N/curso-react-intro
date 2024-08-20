5ed10768c1632d35ec0641d44f374966af827fa0
Natan Castaneda <ncastanedahidalgo@gmail.com>
2024-08-19 20:42:10 -0600

    Publishing to web

--- src/TodosEmpty/index.js
+++ src/TodosEmpty/index.js
@@ -5,7 +5,7 @@ function TodosEmpty(){
     return(
             <div className="card">
                 <div className="card-overlay"></div>
-                <div className="card-inner">YOUR<br>CONTENT</br>HERE</div>
+                <div className="card-inner"></div>
             </div>
     );
 };
